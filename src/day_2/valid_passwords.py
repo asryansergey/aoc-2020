f22e5e5042d9be4b2107d057aac2bbd8551bf37b
Sergey Asryan <asryan.sergey@gmail.com>
2021-02-24 21:29:21 +0400

    SOlved part 2 of day 2.

--- src/day_2/valid_passwords.py
+++ src/day_2/valid_passwords.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Callable
 import re
 import os
 
@@ -8,18 +8,39 @@ def process_input(file_name):
         return [line.strip() for line in report]
 
 
-def get_valid_passvords(pwsd_list: List[int]) -> Optional[int]:
+def is_valid_passwords_in_range(range_min, range_max, symbol, text):
+    if len(re.findall(f"{symbol}", text)) in range(range_min, range_max + 1):
+        return True
+    return False
+
+
+def is_valid_passwords_in_postion(pos_1, pos_2, symbol, text):
+    if any(text[i - 1] == symbol for i in [pos_1, pos_2]) and not all(
+        text[i - 1] == symbol for i in [pos_1, pos_2]
+    ):
+        return True
+    return False
+
+
+def get_valid_passvords(
+    pwsd_list: List[int], count: Callable[..., Optional[int]]
+) -> Optional[int]:
     valid_pswd_count = 0
     for pswd in pwsd_list:
         data = (line for line in pswd.split(" "))
         range_min, range_max = (int(num) for num in re.split(r"\-", next(data)))
         symbol = next(data).split(":")[0]
         text = next(data).strip()
-        if len(re.findall(f"{symbol}", text)) in range(range_min, range_max + 1):
+        if count(range_min, range_max, symbol, text):
             valid_pswd_count += 1
     return valid_pswd_count if valid_pswd_count else None
 
 
 if __name__ == "__main__":
     password_list = process_input(os.path.join("src", "day_2", "input.in"))
-    print(f"Valid passwords count is: {get_valid_passvords(password_list)}")
+    print(
+        f"Valid passwords count is: {get_valid_passvords(password_list, is_valid_passwords_in_range)}"
+    )
+    print(
+        f"Valid passwords count is: {get_valid_passvords(password_list, is_valid_passwords_in_postion)}"
+    )
