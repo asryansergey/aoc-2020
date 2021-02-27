d05594e902378bec5d92584e4275661e791e0abf
Sergey Asryan <asryan.sergey@gmail.com>
2021-02-27 22:20:39 +0400

    Solution for day_3 part 1.

--- src/day_3/toboggan_trajectory.py
+++ src/day_3/toboggan_trajectory.py
@@ -2,11 +2,30 @@ from typing import List, Optional, Callable
 import re
 import os
 
+STEP_RIGHT = 3
+STEP_DOWN = 1
+
 
 def process_input(file_name):
     with open(file_name) as report:
         return [line.strip() for line in report]
 
 
+def next_pos(position: int, mod: int) -> int:
+    return (position + STEP_RIGHT) % mod
+
+
+def count_trees(maze: List[str]) -> int:
+    tree_count = 0
+    check_pos = STEP_RIGHT
+    maze_row_len = len(maze[0])
+    for row in maze[1:]:
+        if row[check_pos] == "#":
+            tree_count += 1
+        check_pos = next_pos(check_pos, maze_row_len)
+    return tree_count
+
+
 if __name__ == "__main__":
     maze = process_input(os.path.join("src", "day_3", "input.in"))
+    print(count_trees(maze))
