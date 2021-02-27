426a0f4b217eb2d42702706a11694150e7530684
Sergey Asryan <asryan.sergey@gmail.com>
2021-02-28 00:16:39 +0400

    Solution for day 3 part 2.

--- src/day_3/toboggan_trajectory.py
+++ src/day_3/toboggan_trajectory.py
@@ -1,9 +1,6 @@
-from typing import List, Optional, Callable
-import re
+from typing import List, Optional, Callable, Tuple
 import os
-
-STEP_RIGHT = 3
-STEP_DOWN = 1
+from functools import reduce
 
 
 def process_input(file_name):
@@ -11,21 +8,23 @@ def process_input(file_name):
         return [line.strip() for line in report]
 
 
-def next_pos(position: int, mod: int) -> int:
-    return (position + STEP_RIGHT) % mod
+def next_pos(position: int, mod: int, step: int) -> int:
+    return (position + step) % mod
 
 
-def count_trees(maze: List[str]) -> int:
+def count_trees(maze: List[str], slope: Tuple) -> int:
     tree_count = 0
-    check_pos = STEP_RIGHT
+    step = slope[0]
+    check_pos = step
     maze_row_len = len(maze[0])
-    for row in maze[1:]:
-        if row[check_pos] == "#":
+    for i in range(0, len(maze) - 1, slope[1]):
+        if maze[i + slope[1]][check_pos] == "#":
             tree_count += 1
-        check_pos = next_pos(check_pos, maze_row_len)
+        check_pos = next_pos(check_pos, maze_row_len, step)
     return tree_count
 
 
 if __name__ == "__main__":
     maze = process_input(os.path.join("src", "day_3", "input.in"))
-    print(count_trees(maze))
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    reduce(lambda x, y: x * y, [count_trees(maze, slope) for slope in slopes])
