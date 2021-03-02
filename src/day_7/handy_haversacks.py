79fb380a14887dfba89def02408f8e3c1a94a771
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-03 01:59:27 +0400

    Add some notes.

--- src/day_7/handy_haversacks.py
+++ src/day_7/handy_haversacks.py
@@ -6,11 +6,24 @@ import itertools
 
 
 def process_input(file_name):
+    """
+    create dict with the following structure:
+    {
+        "bag_name" : [list of bags it contains]
+        ...
+    }
+    First search for "shiny gold" bag, count those bags and
+    adding those in a list for further search e.g.
+    "X" bag containg "shiny gold" bag
+    add "X" bag to a list and after finding all "shiny gold" bag
+    search for bags containing "X" bag and so on untill all bags
+    are scanned.
+    """
     result = []
     with open(file_name) as report:
         return [line.strip() for line in report]
 
 
 if __name__ == "__main__":
-    baggage_list = process_input(os.path.join("src", "day_6", "input.in"))
+    baggage_list = process_input(os.path.join("src", "day_7", "input.in"))
     ...
