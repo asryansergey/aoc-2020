dcab920eb9c91426aee192f9bd461c6f7e49e593
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-02 15:48:37 +0400

    Solution for day 6 part 2.

--- src/day_6/custom_customs.py
+++ src/day_6/custom_customs.py
@@ -8,16 +8,28 @@ import itertools
 def process_input(file_name):
     result = []
     with open(file_name) as report:
-        return [line.replace("\n", "") for line in report.read().split("\n\n")]
+        return [line.replace("\n", " ") for line in report.read().split("\n\n")]
 
 
-def sum_of_yes_answers(answers: List[str]) -> int:
+def sum_of_yes_answers_part_1(answers: List[str]) -> int:
     count_sum = 0
     for line in answers:
-        count_sum += len(set(line))
+        count_sum += len(set(line.replace(" ", "")))
+    return count_sum
+
+
+def sum_of_yes_answers_part_2(answers: List[str]) -> int:
+    count_sum = 0
+    for line in answers:
+        set_list = [set(x) for x in line.split(" ")]
+        intersection = set_list[0]
+        for person_answer in set_list[1:]:
+            intersection = intersection & person_answer
+        count_sum += len(intersection)
     return count_sum
 
 
 if __name__ == "__main__":
     answers = process_input(os.path.join("src", "day_6", "input.in"))
-    print(sum_of_yes_answers(answers))
+    print(sum_of_yes_answers_part_1(answers))
+    print(sum_of_yes_answers_part_2(answers))
