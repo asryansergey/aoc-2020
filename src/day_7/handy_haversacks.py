17cf326a7136baabde6a5f734b889338da01aa2d
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-05 14:32:26 +0400

    Add input processing for day 7.

--- src/day_7/handy_haversacks.py
+++ src/day_7/handy_haversacks.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-import os
+import os, re
 from functools import reduce
 import math
 import itertools
@@ -19,11 +19,23 @@ def process_input(file_name):
     search for bags containing "X" bag and so on untill all bags
     are scanned.
     """
-    result = []
+    result = {}
     with open(file_name) as report:
-        return [line.strip() for line in report]
+        for line in report.readlines():
+            key = line.split("contain")[0].strip()
+            bag_list = [
+                re.split(r"\d ", x)[1]
+                for x in line.split("contain")[1].strip()[:-1].split(",")
+                if re.search(r"\d", x)
+            ]
+            result[key] = bag_list
+        return result
+
+
+def count_shiny_gold_containers(bags: List) -> int:
+    ...
 
 
 if __name__ == "__main__":
     baggage_list = process_input(os.path.join("src", "day_7", "input.in"))
-    ...
+    print(baggage_list)
