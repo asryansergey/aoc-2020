858c5d323f7f4e59228a119863a73040c1c54cd7
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-05 16:35:21 +0400

    Solution for day 7 part 1.

--- src/day_7/handy_haversacks.py
+++ src/day_7/handy_haversacks.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Dict
 import os, re
 from functools import reduce
 import math
@@ -6,25 +6,12 @@ import itertools
 
 
 def process_input(file_name):
-    """
-    create dict with the following structure:
-    {
-        "bag_name" : [list of bags it contains]
-        ...
-    }
-    First search for "shiny gold" bag, count those bags and
-    adding those in a list for further search e.g.
-    "X" bag containg "shiny gold" bag
-    add "X" bag to a list and after finding all "shiny gold" bag
-    search for bags containing "X" bag and so on untill all bags
-    are scanned.
-    """
     result = {}
     with open(file_name) as report:
         for line in report.readlines():
-            key = line.split("contain")[0].strip()
+            key = line.split("contain")[0].split("bag")[0].strip()
             bag_list = [
-                re.split(r"\d ", x)[1]
+                re.split(r"\d ", x)[1].split("bag")[0].strip()
                 for x in line.split("contain")[1].strip()[:-1].split(",")
                 if re.search(r"\d", x)
             ]
@@ -32,10 +19,22 @@ def process_input(file_name):
         return result
 
 
+def find_shiny_gold(search_list: List[str], bags_dict: Dict) -> int:
+    for idx, elem in enumerate(search_list):
+        for k, bags in bags_dict.items():
+            if elem in bags:
+                search_list.append(k)
+                bags_dict.pop(k)
+                return find_shiny_gold(search_list, bags_dict) + 1
+        search_list.remove(elem)
+    return 0
+
+
 def count_shiny_gold_containers(bags: List) -> int:
-    ...
+    search_list = ["shiny gold"]
+    return find_shiny_gold(search_list, bags)
 
 
 if __name__ == "__main__":
     baggage_list = process_input(os.path.join("src", "day_7", "input.in"))
-    print(baggage_list)
+    print(count_shiny_gold_containers(baggage_list))
