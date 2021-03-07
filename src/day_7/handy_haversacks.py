99fd75e55b040878311eb108853ebb1d8301f1a6
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-07 23:05:23 +0400

    Solution for day 7 part 2.

--- src/day_7/handy_haversacks.py
+++ src/day_7/handy_haversacks.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import os, re
 from functools import reduce
 import math
@@ -11,7 +11,10 @@ def process_input(file_name):
         for line in report.readlines():
             key = line.split("contain")[0].split("bag")[0].strip()
             bag_list = [
-                re.split(r"\d ", x)[1].split("bag")[0].strip()
+                (
+                    int(re.split(r"(\d )", x)[1].strip()),
+                    re.split(r"(\d )", x)[2].split("bag")[0].strip(),
+                )
                 for x in line.split("contain")[1].strip()[:-1].split(",")
                 if re.search(r"\d", x)
             ]
@@ -19,10 +22,10 @@ def process_input(file_name):
         return result
 
 
-def find_shiny_gold(search_list: List[str], bags_dict: Dict) -> int:
-    for idx, elem in enumerate(search_list):
+def find_shiny_gold(search_list: List[str], bags_dict: Dict[str, List[Tuple]]) -> int:
+    for elem in search_list:
         for k, bags in bags_dict.items():
-            if elem in bags:
+            if [x for x in bags if elem in x[1]]:
                 search_list.append(k)
                 bags_dict.pop(k)
                 return find_shiny_gold(search_list, bags_dict) + 1
@@ -30,11 +33,29 @@ def find_shiny_gold(search_list: List[str], bags_dict: Dict) -> int:
     return 0
 
 
-def count_shiny_gold_containers(bags: List) -> int:
+def find_bags_inside_shiny_gold(search_list: List, bags_dict: Dict) -> int:
+    result = 0
+    while len(search_list):
+        child_sum = result
+        root = search_list.pop(0)
+        queue = bags_dict[root[1]]
+        for elem in queue:
+            child_sum += elem[0] * (find_bags_inside_shiny_gold([elem], bags_dict) + 1)
+        result += child_sum
+    return result
+
+
+def count_shiny_gold_containers(bags_dict: Dict) -> int:
     search_list = ["shiny gold"]
-    return find_shiny_gold(search_list, bags)
+    return find_shiny_gold(search_list, bags_dict)
+
+
+def count_bags_inside_shiny_gold(bags_dict: Dict) -> int:
+    search_list = [(1, "shiny gold")]
+    return find_bags_inside_shiny_gold(search_list, bags_dict)
 
 
 if __name__ == "__main__":
     baggage_list = process_input(os.path.join("src", "day_7", "input.in"))
     print(count_shiny_gold_containers(baggage_list))
+    print(count_bags_inside_shiny_gold(baggage_list))
