a8e0dd5d85fed0bbd29f9796650508e785501395
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-09 23:44:30 +0400

    Solution for day 8 part 1.

--- src/day_8/handheld_halting.py
+++ src/day_8/handheld_halting.py
@@ -4,14 +4,37 @@ from functools import reduce
 import math
 import itertools
 
+VALID_INSTRUCTIONS = {
+    "acc": lambda x, y: (x + y, 1),
+    "jmp": lambda x, y: (x, y),
+    "nop": lambda x, y: (x, 1),
+}
+
 
 def process_input(file_name):
     with open(file_name) as report:
-        return [line.strip() for line in report]
+        result = []
+        for line in report:
+            instr, arg = line.strip().split(" ")
+            result.append((instr, int(arg)))
+        return result
 
 
-def find_accumulator(instr: List) -> int:
-    ...
+def find_accumulator(instructions: List) -> int:
+    accumulator = 0
+    visited_instructions = set()
+    instr_ptr = 0
+    for _ in instructions:
+        instr = instructions[instr_ptr]
+        cmd, arg = instr
+        if (cmd, arg, instr_ptr) in visited_instructions:
+            return accumulator
+        visited_instructions.add((*instr, instr_ptr))
+        if not cmd in VALID_INSTRUCTIONS:
+            raise Exception(f"Invalid Instruction {cmd} in a list")
+        accumulator, ip_value = VALID_INSTRUCTIONS[cmd](accumulator, arg)
+        instr_ptr += ip_value
+    return accumulator
 
 
 if __name__ == "__main__":
