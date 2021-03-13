176b334ff8701a6c021ed027743e1de496e7270f
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-13 12:34:08 +0400

    Convert input to int and add can_sum function.

--- src/day_9/encoding_error.py
+++ src/day_9/encoding_error.py
@@ -4,7 +4,11 @@ import os
 
 def process_input(file_name):
     with open(file_name) as report:
-        return [line.strip() for line in report]
+        return [int(line.strip()) for line in report]
+
+
+def can_sum(preamble: List[int], target_num):
+    ...
 
 
 def find_target_number(number_list: List) -> int:
@@ -13,4 +17,5 @@ def find_target_number(number_list: List) -> int:
 
 if __name__ == "__main__":
     number_list = process_input(os.path.join("src", "day_9", "input.in"))
+    print(number_list)
     print(find_target_number(number_list))
