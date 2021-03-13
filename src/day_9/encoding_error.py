2cc651c809b7dc0ac851b042a89f1878b415ab48
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-13 14:11:09 +0400

    Solution for day 9 part 2.

--- src/day_9/encoding_error.py
+++ src/day_9/encoding_error.py
@@ -1,6 +1,5 @@
-from typing import List, Optional, Callable
+from typing import List, Optional
 import os
-from functools import wraps
 
 PREAMBLE_RANGE = 25
 
@@ -22,7 +21,7 @@ def can_sum(preamble: List[int], target_num: int):
     return False
 
 
-def find_target_number(number_list: List) -> Optional[int]:
+def find_target_number(number_list: List[int]) -> Optional[int]:
     for idx in range(len(number_list[:-PREAMBLE_RANGE])):
         preamble = number_list[idx : idx + PREAMBLE_RANGE]
         target_num = number_list[idx + PREAMBLE_RANGE]
@@ -31,6 +30,30 @@ def find_target_number(number_list: List) -> Optional[int]:
     return None
 
 
+def find_xmas_weakness(number_list: List[int], target_number: int) -> int:
+    target_index = number_list.index(target_number)
+    start_pos = 0
+    while start_pos < target_index:
+        contiguous_list = [number_list[start_pos]]
+        for idx in range(start_pos + 1, len(number_list[:target_index])):
+            num = number_list[idx]
+            contiguous_list.append(num)
+            total_sum = sum(contiguous_list)
+            if num >= target_number:
+                contiguous_list.clear()
+                continue
+            if total_sum > target_number:
+                contiguous_list = [contiguous_list[-1]]
+                continue
+            if total_sum == target_number:
+                contiguous_list.sort()
+                return contiguous_list[0] + contiguous_list[-1]
+        start_pos += 1
+    return None
+
+
 if __name__ == "__main__":
     number_list = process_input(os.path.join("src", "day_9", "input.in"))
-    print(find_target_number(number_list))
+    target_number = find_target_number(number_list)
+    print(target_number)
+    print(find_xmas_weakness(number_list, target_number))
