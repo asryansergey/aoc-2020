eb36ef60a591d3e9fad28d067f3310df651a3f69
Sergey Asryan <asryan.sergey@gmail.com>
2021-03-19 18:14:30 +0400

    Optimisation for day 10 part 1 and solution for part 2.

--- src/day_10/adapter_array.py
+++ src/day_10/adapter_array.py
@@ -14,18 +14,35 @@ def find_target_number(jolts_list: List[int]) -> Optional[int]:
     jolts_list.sort()
     built_in_joltage = jolts_list[-1]
     jolts_diff_result[3] += [built_in_joltage]
-    for diff, _ in enumerate(jolts_diff_result.items(), 1):
-        for idx, elem in enumerate(jolts_list, 1):
-            if idx >= len(jolts_list):
-                break
-            if jolts_list[idx] - elem == diff:
-                jolts_diff_result[diff].append(jolts_list[idx])
+    for idx, elem in enumerate(jolts_list, 1):
+        if idx >= len(jolts_list):
+            break
+        diff = jolts_list[idx] - elem
+        jolts_diff_result[diff].append(jolts_list[idx])
 
     return len(jolts_diff_result[1]) * len(jolts_diff_result[3])
 
 
+def adapters_distinct_arrangement_count(jolts_list: List[int]) -> int:
+    jolts_diff_result = [1]
+    jolts_list.sort()
+    built_in_joltage_rate = jolts_list[-1] + 3
+    for idx in range(1, built_in_joltage_rate + 1):
+        if not idx in jolts_list and idx != built_in_joltage_rate:
+            jolts_diff_result.append(0)
+        else:
+            curr_val = jolts_diff_result[idx - 1]
+            if idx >= 2:
+                curr_val += jolts_diff_result[idx - 2]
+            if idx >= 3:
+                curr_val += jolts_diff_result[idx - 3]
+            jolts_diff_result.append(curr_val)
+
+    return jolts_diff_result[-1]
+
+
 if __name__ == "__main__":
     jolts_list = process_input(os.path.join("src", "day_10", "input.in"))
-    null_jolt = [0]
-    jolts_list += null_jolt
-    print(find_target_number(jolts_list))
+    null_joltage_rate = [0]
+    print(find_target_number(jolts_list + null_joltage_rate))
+    print(adapters_distinct_arrangement_count(jolts_list))
